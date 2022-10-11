4e46c3da6d9039f05583af76f0d6691f0c75247c
Murilo Pereira <contato@murilopereira.dev.br>
2022-10-11 00:53:06 -0300

    feat: Layout Tela Inicial 1.0 Finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:pokedex_secompp/classes/pokemon.dart';
 import 'package:pokedex_secompp/components/pokemon_card.dart';
 import 'package:pokedex_secompp/network.dart';
@@ -47,7 +50,7 @@ class PokemonList extends StatefulWidget {
 }
 
 class _PokemonListState extends State<PokemonList> {
-  ScrollController? _scrollController;
+  ScrollController? _scrollController;  
   final List<Pokemon> _pokeList = [];
   int _page = 0;
   bool _isLoading = false;
@@ -55,7 +58,7 @@ class _PokemonListState extends State<PokemonList> {
   @override
   void initState() {     
     _scrollController = ScrollController(initialScrollOffset: 5.0)
-    ..addListener(_scrollListener);    
+    ..addListener(_scrollListener);   
 
     super.initState();
   }
@@ -97,18 +100,67 @@ class _PokemonListState extends State<PokemonList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
-        child: GridView.count(
-          crossAxisCount: 2,
-          controller: _scrollController,
-          scrollDirection: Axis.vertical,
-          mainAxisSpacing: 10.0,
-          crossAxisSpacing: 10.0,
-          physics: const AlwaysScrollableScrollPhysics(),
-          children: _pokeList.map((e) => PokemonCard(pokemon: e)).toList()
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 15.0, left: 10.0),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 30.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Pokedex',
+                          style: GoogleFonts.montserrat(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 28,
+                            color: Theme.of(context).colorScheme.secondary
+                          ),
+                        ),
+                      ]
+                    )
+                  ),
+                  Expanded(                  
+                    child: Stack(            
+                      fit: StackFit.passthrough,
+                      children: [              
+                        Positioned(                    
+                          right: 0,
+                          bottom: 0,
+                          child: Image.asset(
+                            "images/pokeball.png",
+                            scale: .75,
+                          )
+                        )
+                      ]
+                    )
+                  )
+                ]
+              )
+            )
+          ),
+          Expanded(
+            flex: 5,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+              child: GridView.count(
+                crossAxisCount: 2,
+                controller: _scrollController,
+                scrollDirection: Axis.vertical,
+                mainAxisSpacing: 10.0,
+                crossAxisSpacing: 10.0,
+                padding: EdgeInsets.zero,
+                physics: const AlwaysScrollableScrollPhysics(),
+                children: _pokeList.map((e) => PokemonCard(pokemon: e)).toList()
+              ),
+            )
+          )
+        ]
       )
     );
   }
